8bd64adc30eb779f95a843e8d2ebb18e730e23d6
brunopecampos <brunopecampos@protonmail.com>
2023-06-06 15:56:36 -0300

    Add register endpoint
    Signed-off-by: brunopecampos <brunopecampos@protonmail.com>

--- backend/app.py
+++ backend/app.py
@@ -3,7 +3,7 @@ from flask_bcrypt import Bcrypt
 from flask_cors import CORS, cross_origin
 from flask_session import Session
 from config import ApplicationConfig
-from models import db, User
+from models import db, Hospital, Provider, User
 
 app = Flask(__name__)
 app.config.from_object(ApplicationConfig)
@@ -29,31 +29,47 @@ def get_current_user():
         "email": user.email
     }) 
 
-@app.route("/register", methods=["POST"])
-def register_user():
-    #name = request.json["name"]
-    #cpnj = request.json["cnpj"]
-    #employee_name = request.json["employee_name"]
-    #address = request.json["address"]
-    email = request.json["email"]
-    password = request.json["password"]
-
-    user_exists = er.query.filter_by(email=email).first() is not None
+def add_user(user, new_user):
+    user_exists = user.query.filter_by(email=new_user.email).first() is not None
 
     if user_exists:
         return jsonify({"error": "User already exists"}), 409
 
-    hashed_password = bcrypt.generate_password_hash(password)
-    new_user = User(email=email, password=hashed_password)
     db.session.add(new_user)
     db.session.commit()
-    
+
     session["user_id"] = new_user.id
 
     return jsonify({
         "id": new_user.id,
-        "email": new_user.email
     })
+    
+
+@app.route("/register-hospital", methods=["POST"])
+def register_hospital():
+    name = request.json["name"]
+    cnpj = request.json["cnpj"]
+    employee_name = request.json["employee_name"]
+    address = request.json["address"]
+    email = request.json["email"]
+    password = request.json["password"]
+
+    hashed_password = bcrypt.generate_password_hash(password)
+    new_hospital = Hospital(name=name, cnpj=cnpj, employee_name=employee_name, address=address, email=email, password=hashed_password)
+    return add_user(Hospital, new_hospital)
+
+@app.route("/register-provider", methods=["POST"])
+def register_provider():
+    name = request.json["name"]
+    cnpj = request.json["cnpj"]
+    employee_name = request.json["employee_name"]
+    address = request.json["address"]
+    email = request.json["email"]
+    password = request.json["password"]
+
+    hashed_password = bcrypt.generate_password_hash(password)
+    new_provider = Provider(name=name, cnpj=cnpj, employee_name=employee_name, address=address, email=email, password=hashed_password)
+    return add_user(Provider, new_provider)
 
 @app.route("/login", methods=["POST"])
 def login_user():
